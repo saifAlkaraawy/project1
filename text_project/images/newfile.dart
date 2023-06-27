613d66563f9281aa4aa6e832dae3753344a8690e
saif <saif@gmail.com>
2023-06-27 16:42:18 +0300

    second commit

---
+++ text_project/images/newfile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class  Mynewfile extends StatefulWidget {
+  const  Mynewfile({super.key});
+
+  @override
+  State  Mynewfile> createState() =>   MynewfileState();
+}
+
+class   MynewfileState extends State  Mynewfile> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
