607b3376a6a8ba68db4a2b02af2dea448ef4f0b5
saif <saif@gmail.com>
2023-06-27 17:18:58 +0300

    sove problem in newFile,dart

--- text_project/images/newfile.dart
+++ text_project/images/newfile.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
-class  Mynewfile extends StatefulWidget {
-  const  Mynewfile({super.key});
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
   @override
-  State  Mynewfile> createState() =>   MynewfileState();
+  State<MyWidget> createState() => _MyWidgetState();
 }
 
-class   MynewfileState extends State  Mynewfile> {
+class _MyWidgetState extends State<MyWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
